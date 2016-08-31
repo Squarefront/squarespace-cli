e3881395ddc061d32f05f9679cbc5b7f7ddb4686
Jason Barone <jason@jasonbarone.com>
2016-08-31 08:38:43 -0700

    Update namespace.

--- commands/index.js
+++ commands/index.js
@@ -5,11 +5,16 @@
  * @description Holds utilities.
  *
  */
+const authentication = require( "./authentication" );
+const createSite = require( "./create-site" );
 const exportPage = require( "./export" );
 const sitemap = require( "./sitemap" );
 
 
 module.exports = {
+  authentication,
+  "create-site": createSite.createSite,
+  "delete-credentials": authentication.deleteStoredAccounts,
   "export": exportPage.exportPage,
   "export-sitemap": sitemap.exportSitemap
 };
